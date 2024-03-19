37936281ffec6c50b13ebeb283e04758e7199dde
Johnny Jiang <zhiyangjjiang@gmail.com>
2024-03-19 10:29:32 -0500

    separated authentication info to txt file

--- build/index.js
+++ build/index.js
@@ -12,23 +12,17 @@ const neo4j = require('neo4j-driver');
 
 (async () => {
   require('dotenv').config({
-    // path: 'Neo4j-b04d4356-Created-2024-03-12.txt',
+    path: 'Neo4j-b04d4356-Created-2024-03-12.txt',
     debug: true  // to raise file/parsing errors
   })
 
-//   const URI = process.env.NEO4J_URI
-//   const USER = process.env.NEO4J_USERNAME
-//   const PASSWORD = process.env.NEO4J_PASSWORD
-  const URI = "neo4j+s://b04d4356.databases.neo4j.io"
-  const USER = "neo4j";
-  const PASSWORD = "VurX_9LR9YcxflvBdcyUrLgFJJO9G3hvoSObJiKIKbA"
+  const URI = process.env.NEO4J_URI
+  const USER = process.env.NEO4J_USERNAME
+  const PASSWORD = process.env.NEO4J_PASSWORD
 
   let driver
 
   try {
-    console.log(`${URI}`)
-    console.log(`${USER}`)
-    console.log(`${PASSWORD}`)
     driver = neo4j.driver(URI,  neo4j.auth.basic(USER, PASSWORD))
     const serverInfo = await driver.getServerInfo()
     console.log('Connection estabilished')
