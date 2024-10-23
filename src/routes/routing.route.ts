1ad59ada06fa94f313d1d79904c0e4d25fb8eb9f
avik-ch <arkaavik@outlook.com>
2024-10-22 21:06:41 -0500

    trying documentation

--- src/routes/routing.route.ts
+++ src/routes/routing.route.ts
@@ -3,6 +3,13 @@ import { buildingRouting, popularRoutes } from '../controller/routing.controller
 
 const router = express.Router();
 
+/** 
+ * @openapi
+ * /route:
+ *   get:
+ *     summary: some summary
+ *     description: some description
+*/
 router.get('/route?', buildingRouting);
 router.get('/popular', popularRoutes);
 router.post('/setroute')
